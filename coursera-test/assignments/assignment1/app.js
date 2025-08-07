3e8655f8c5f7878f9ef9d92c2d4d31be31e478d0
d685366 <david.jurio.perez@navarra.es>
2025-08-07 14:10:55 +0200

    hacer el código tolerante a la minización de código

--- coursera-test/assignments/assignment1/app.js
+++ coursera-test/assignments/assignment1/app.js
@@ -1,25 +1,29 @@
 (function () {
     'use strict';
 
+    // Definición del controlador
+    function Assignment1Controller($scope) {
+        $scope.dishes = "";
+        $scope.input = "";
+
+        $scope.buttonClicked = function () {
+            console.log($scope.input);
+
+            if ($scope.input === "") {
+                $scope.dishes = "Empty";
+            } else if ($scope.input.split(",").length <= 3) {
+                $scope.dishes = "Enjoy!";
+            } else {
+                $scope.dishes = "Too much!";
+            }
+        };
+    }
+
+    // Inyección de dependencias explícita
+    Assignment1Controller.$inject = ['$scope'];
+
+    // Registro del módulo y controlador
     angular.module('assignment1App', [])
-        .controller('Assignment1Controller', function ($scope) {
-            $scope.dishes = "";
-            $scope.input = "Inicializado";
-            $scope.buttonClicked = function () {
-                console.log($scope.input)
-                if ($scope.input === "") {
-                    $scope.dishes = "Empty";
-                }
-                else if ($scope.input.split(",").length <= 3) {
-                    $scope.dishes = "Enjoy!";
-                    
-                }
-                else {
-                    $scope.dishes = "Too much!";
-                }
-
-            };
-
-        });
+        .controller('Assignment1Controller', Assignment1Controller);
 
 })();
