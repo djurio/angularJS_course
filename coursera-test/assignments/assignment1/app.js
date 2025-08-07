0ba165f9e70216eb18902f8c4d843b4d8666c22f
d685366 <david.jurio.perez@navarra.es>
2025-08-07 14:58:55 +0200

    corrección de mensaje

--- coursera-test/assignments/assignment1/app.js
+++ coursera-test/assignments/assignment1/app.js
@@ -1,7 +1,6 @@
 (function () {
     'use strict';
 
-<<<<<<< HEAD
     // Definición del controlador
     function Assignment1Controller($scope) {
         $scope.dishes = "";
@@ -9,27 +8,9 @@
 
         $scope.buttonClicked = function () {
             console.log($scope.input);
-=======
-    angular.module('assignment1App', [])
-        .controller('Assignment1Controller', function ($scope) {
-            $scope.dishes = "";
-            $scope.input = "";
-            $scope.buttonClicked = function () {
-                console.log($scope.input)
-                if ($scope.input === "") {
-                    $scope.dishes = "Empty";
-                }
-                else if ($scope.input.split(",").length <= 3) {
-                    $scope.dishes = "Enjoy!";
-                    
-                }
-                else {
-                    $scope.dishes = "Too much!";
-                }
->>>>>>> b7778682e8465bf8afe070517764c0eb2f938d59
 
             if ($scope.input === "") {
-                $scope.dishes = "Empty";
+                $scope.dishes = "Please enter data first";
             } else if ($scope.input.split(",").length <= 3) {
                 $scope.dishes = "Enjoy!";
             } else {
